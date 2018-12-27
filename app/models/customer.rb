6cd3334f39d9899cd01737e517f5c68e31de7316
momoko <ne290179@senshu-u.jp>
2018-12-27 15:42:57 +0900

    [add]customerモデルにバリデーションを追加
    [memo]エラーメッセージが日本語化されていません

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,17 @@
 class Customer < ActiveRecord::Base
   has_many :orders, dependent: :destroy
+  validates :name, presence: true,
+    length: {minumum: 1,maximum: 20,allow_blank: true}
+  validates :telephone, presence: true,
+    format: { with: /[0-9()-]/, allow_blank: true },
+    length: { maximum: 15, allow_blank: true }
+  validates :username, presence: true,
+    format: { with: /\A[A-Za-z]\w*\z/, allow_blank: true},
+    length: { minimum: 3, maximum: 20, allow_blank: true},
+    uniqueness: {case_sensitive: false}
+  validates :mail_address, presence: true,
+    length: { minimum: 1, allow_blank: true }
+
   validates :password, presence: {on: :create},
      confirmation: {allow_blank: true} 
   attr_accessor :password, :password_confirmation
